f710bed64602859d92bef4519c8045d1314c4ab3
Maz <romainmazb@gmail.com>
2022-07-21 16:02:35 +0200

    add ball and right pad

--- script.js
+++ script.js
@@ -8,10 +8,23 @@ const playerLeftPad = {
     width: 25,
     height: 100
 }
+const playerRightPad = {
+    x: 0,
+    y: 0,
+    width: 25,
+    height: 100
+}
 
 const screen = {
     width: 0,
     height: 0
+}
+
+const ball = {
+    x: 0,
+    y: 0,
+    width: 16,
+    height: 16
 }
 
 /**
@@ -23,6 +36,12 @@ function LoadGame(canvas, context) {
 
     playerLeftPad.x = 10
     playerLeftPad.y = screen.height / 2 - playerLeftPad.height / 2
+
+    playerRightPad.x = screen.width - 10 - playerRightPad.width
+    playerRightPad.y = screen.height / 2 - playerRightPad.height / 2
+
+    ball.x = screen.width / 2 - ball.width / 2
+    ball.y = screen.height / 2 - ball.height / 2
 }
 
 /**
@@ -36,5 +55,12 @@ function UpdateGame(deltaTime) {
  * Exécutée perpétuellement pour dessiner la frame actuelle
  */
 function DrawGame(context) {
+    // Dessine la raquette de gauche
     context.fillRect(playerLeftPad.x, playerLeftPad.y, playerLeftPad.width, playerLeftPad.height)
+
+    // Dessine la raquette de droite
+    context.fillRect(playerRightPad.x, playerRightPad.y, playerRightPad.width, playerRightPad.height)
+
+    // Dessine la balle
+    context.fillRect(ball.x, ball.y, ball.width, ball.height)
 }
